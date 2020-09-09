74e8eec2fbe13f2fe7cecde7182713094d31efc2
gusgluna <gusgluna@gmail.com>
2020-09-09 12:20:17 -0500

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,3 +1,5 @@
+import random
+
 bloque01 = {
   'a1' : 1, 'b1' : 2, 'c1' : 3,
   'a2' : 4, 'b2' : 5, 'c2' : 6
@@ -5,3 +7,6 @@ bloque01 = {
 
 for numeros in bloque01.values():
   print(numeros,end="")
+
+rand = random.randint(0,10)
+print(rand)
