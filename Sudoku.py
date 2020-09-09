a8fe87f3cf5705a12a3421c4182f9d346c4a7739
gusgluna <gusgluna@gmail.com>
2020-09-09 12:13:42 -0500

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,4 +1,7 @@
-bloque01 = {'a1' : 1, 'b1' : 2, 'b3' : 3}
+bloque01 = {
+  'a1' : 1, 'b1' : 2, 'c1' : 3,
+  'a2' : 4, 'b2' : 5, 'c2' : 6
+}
 
 for numeros in bloque01.values():
-  print(numeros)
+  print(numeros,end="")
