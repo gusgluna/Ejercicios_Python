288124fff17d09a7b1cfbab072941da1c96aca95
gusgluna <gusgluna@gmail.com>
2020-09-09 22:51:03 -0500

    gusgluna

--- Sudoku/Sudoku.py
+++ Sudoku/Sudoku.py
@@ -1,8 +1,8 @@
 import random
 
 bloque01 = {
-  'a1' : 1, 'b1' : 2, 'c1' : 3,
-  'a2' : 4, 'b2' : 5, 'c2' : 6
+  'a1' : '', 'b1' : '', 'c1' : '',
+  'a2' : '', 'b2' : '', 'c2' : '',
 }
 
 for numeros in bloque01.values():
