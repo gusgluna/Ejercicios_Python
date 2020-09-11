510098aaa2c897d68d40e14f9d5eed76136605b1
gusgluna <gusgluna@gmail.com>
2020-09-11 17:07:04 -0500

    Update Sudoku.py

--- Sudoku/Sudoku.py
+++ Sudoku/Sudoku.py
@@ -1,9 +1,9 @@
 import random
 
 bloque_01 = {
-  'a1' : 0, 'b1' : 0, 'c1' : 0, 'd1' : 0, 'e1' : 0, 'f1' : 0, 'g1' : 1, 'h1' : 2, 'i1' : 3,
-  'a2' : 0, 'b2' : 0, 'c2' : 0, 'd2' : 0, 'e2' : 0, 'f2' : 0,
-  'a3' : 0, 'b3' : 0, 'c3' : 0, 'd3' : 0, 'e3' : 0, 'f3' : 0,
+  'a1' : 0, 'b1' : 0, 'c1' : 0, 'd1' : 0, 'e1' : 0, 'f1' : 0, 'g1' : 0, 'h1' : 0, 'i1' : 0,
+  'a2' : 0, 'b2' : 0, 'c2' : 0, 'd2' : 0, 'e2' : 0, 'f2' : 0, 'g2' : 0, 'h2' : 0, 'i2' : 0,
+  'a3' : 0, 'b3' : 0, 'c3' : 0, 'd3' : 0, 'e3' : 0, 'f3' : 0, 'g3' : 0, 'h3' : 0, 'i3' : 0,
 }
 
 #Definiendo el valor de a1
@@ -143,11 +143,24 @@ while num_rand in tup_comp:
   num_rand = random.randint(1,9)
 bloque_01['f3'] = num_rand
 
+#Definiendo el valor de g1
+tup_comp = (bloque_01['a1'], bloque_01['b1'],
+bloque_01['c1'], bloque_01['d1'], bloque_01['e1'], bloque_01['f1'])
+num_rand = random.randint(1,9)
+while num_rand in tup_comp:
+  num_rand = random.randint(1,9)
+bloque_01['g1'] = num_rand
+
+
 print('  a b c d e f g h i')
+
 print(' ╔═════╦═════╦═════╗')
 print('1║'+str(bloque_01['a1'])+'│'+str(bloque_01['b1'])+'│'+str(bloque_01['c1'])+'║'+str(bloque_01['d1'])+'│'+str(bloque_01['e1'])+'│'+str(bloque_01['f1'])+'║'+str(bloque_01['g1'])+'│'+str(bloque_01['h1'])+'│'+str(bloque_01['i1'])+'║')
 
-print('2║'+str(bloque_01['a2'])+'│'+str(bloque_01['b2'])+'│'+str(bloque_01['c2'])+'║'+str(bloque_01['d2'])+'│'+str(bloque_01['e2'])+'│'+str(bloque_01['f2'])+'║')
+print('2║'+str(bloque_01['a2'])+'│'+str(bloque_01['b2'])+'│'+str(bloque_01['c2'])+'║'+str(bloque_01['d2'])+'│'+str(bloque_01['e2'])+'│'+str(bloque_01['f2'])+'║'+str(bloque_01['g2'])+'│'+str(bloque_01['h2'])+'│'+str(bloque_01['i2'])+'║')
 
-print('3║'+str(bloque_01['a3'])+'│'+str(bloque_01['b3'])+'│'+str(bloque_01['c3'])+'║'+str(bloque_01['d3'])+'│'+str(bloque_01['e3'])+'│'+str(bloque_01['f3'])+'║')
-print(' ╚═════╬═════╬')
+print('3║'+str(bloque_01['a3'])+'│'+str(bloque_01['b3'])+'│'+str(bloque_01['c3'])+'║'+str(bloque_01['d3'])+'│'+str(bloque_01['e3'])+'│'+str(bloque_01['f3'])+'║'+str(bloque_01['g3'])+'│'+str(bloque_01['h3'])+'│'+str(bloque_01['i3'])+'║')
+
+print(' ╠═════╬═════╬═════╣')
+
+print('4║')
