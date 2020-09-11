ffe03002d513eb83f81881e9db60c1b61cf594b5
gusgluna <gusgluna@gmail.com>
2020-09-11 13:15:50 -0500

    Update Sudoku.py

--- Sudoku/Sudoku.py
+++ Sudoku/Sudoku.py
@@ -1,9 +1,9 @@
 import random
 
 bloque_01 = {
-  'a1' : 0, 'b1' : 0, 'c1' : 0, 'd1' : 1, 'e1' : 2, 'f1' : 3,
-  'a2' : 0, 'b2' : 0, 'c2' : 0, 'd2' : 4, 'e2' : 5, 'f2' : 6,
-  'a3' : 0, 'b3' : 0, 'c3' : 0, 'd3' : 7, 'e3' : 8, 'f3' : 9,
+  'a1' : 0, 'b1' : 0, 'c1' : 0, 'd1' : 0, 'e1' : 0, 'f1' : 0,
+  'a2' : 0, 'b2' : 0, 'c2' : 0, 'd2' : 0, 'e2' : 0, 'f2' : 0,
+  'a3' : 0, 'b3' : 0, 'c3' : 0, 'd3' : 0, 'e3' : 0, 'f3' : 0,
 }
 
 #Definiendo el valor de a1
@@ -71,7 +71,37 @@ while num_rand in tup_comp:
   num_rand = num_rand + 1
 bloque_01['c3'] = num_rand
 
+#Definiendo el valor de d1
+tup_comp = (bloque_01['a1'], bloque_01['b1'],
+bloque_01['c1'])
+num_rand = random.randint(1,9)
+while num_rand in tup_comp:
+  num_rand = random.randint(1,9)
+bloque_01['d1'] = num_rand
 
+#Definiendo el valor de e1
+tup_comp = (bloque_01['a1'], bloque_01['b1'],
+bloque_01['c1'], bloque_01['d1'])
+num_rand = random.randint(1,9)
+while num_rand in tup_comp:
+  num_rand = random.randint(1,9)
+bloque_01['e1'] = num_rand
+
+#Definiendo el valor de f1
+tup_comp = (bloque_01['a1'], bloque_01['b1'],
+bloque_01['c1'], bloque_01['d1'], bloque_01['e1'])
+num_rand = random.randint(1,9)
+while num_rand in tup_comp:
+  num_rand = random.randint(1,9)
+bloque_01['f1'] = num_rand
+
+#Definiendo el valor de d2
+tup_comp = (bloque_01['a2'], bloque_01['b2'],
+bloque_01['c2'], bloque_01['d1'], bloque_01['e1'], bloque_01['f1'])
+num_rand = random.randint(1,9)
+while num_rand in tup_comp:
+  num_rand = random.randint(1,9)
+bloque_01['d2'] = num_rand
 
 
 print('╔═════╦═════╦')
