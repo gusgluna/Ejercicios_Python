78975c3a6507f25d467e27dcf0572e8d35b98cce
gusgluna <gusgluna@gmail.com>
2020-09-11 11:52:36 -0500

    Update Sudoku.py

--- Sudoku/Sudoku.py
+++ Sudoku/Sudoku.py
@@ -1,9 +1,18 @@
-import random
+#import random
 
-bloque01 = {
-  'a1' : '', 'b1' : '', 'c1' : '', 
-  'a2' : '', 'b2' : '', 'c2' : '',
+bloque_01 = {
+  'a1' : 1, 'b1' : 2, 'c1' : 3, 
+  'a2' : 4, 'b2' : 5, 'c2' : 6,
+  'a3' : 7, 'b3' : 8, 'c3' : 9,
 }
 
-rand = random.randint(0,10)
-print(rand)
+
+print('═════╦')
+print(str(bloque_01['a1'])+'│'+str(bloque_01['b1'])+'│'+str(bloque_01['c1'])+'║')
+
+print(str(bloque_01['a2'])+'│'+str(bloque_01['b2'])+'│'+str(bloque_01['c2'])+'║')
+
+print(str(bloque_01['a3'])+'│'+str(bloque_01['b3'])+'│'+str(bloque_01['c3'])+'║')
+
+"""rand = random.randint(1,9)
+print(rand)"""
