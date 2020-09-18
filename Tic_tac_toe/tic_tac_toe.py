4f74011cecd9f93821f66a22074ab0b5756b101e
gusgluna <gusgluna@gmail.com>
2020-09-18 18:08:52 -0500

    Update tic_tac_toe.py

--- Tic_tac_toe/tic_tac_toe.py
+++ Tic_tac_toe/tic_tac_toe.py
@@ -1,11 +1,5 @@
 import os
 import time
- #Funcion que detecta el sistema operativo y borra la consola
-def borrarPantalla():
-  if os.name == "posix":
-    os.system ("clear")
-  elif os.name == "ce" or os.name == "nt" or os.name == "dos":
-    os.system ("cls")
 
 gato = {
   "1" : "-", "2" : "-", "3" : "-", 
@@ -13,15 +7,30 @@ gato = {
   "7" : "-", "8" : "-", "9" : "-", 
 }
 
-print('Titc-Tac-Toe (Gato)')
-print(' Juego   Posiciones')
-print('════════════════════')
-print(' '+ gato["1"] + '│'+ gato["2"] + '│' + gato["3"]+ "   1│2│3")
-print(' ─┼─┼─   ─┼─┼─')
-print(' '+ gato["4"] + '│'+ gato["5"] + '│' + gato["6"]+ "   4│5│6")
-print(' ─┼─┼─   ─┼─┼─')
-print(' '+ gato["7"] + '│'+ gato["8"] + '│' + gato["9"]+ "   7│8│9")
+
+def app():
+  imprimirjuego()
+
+#Funcion que detecta el sistema operativo y borra la consola
+def borrarPantalla():
+  if os.name == "posix":
+    os.system ("clear")
+  elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+    os.system ("cls")
+
+def imprimirjuego():
+  print('Titc-Tac-Toe (Gato)')
+  print(' Juego   Posiciones')
+  print('════════════════════')
+  print(' '+ gato["1"] + '│'+ gato["2"] + '│' + gato["3"]+ "   1│2│3")
+  print(' ─┼─┼─   ─┼─┼─')
+  print(' '+ gato["4"] + '│'+ gato["5"] + '│' + gato["6"]+ "   4│5│6")
+  print(' ─┼─┼─   ─┼─┼─')
+  print(' '+ gato["7"] + '│'+ gato["8"] + '│' + gato["9"]+ "   7│8│9")
 
 #time.sleep(5)
 
 #borrarPantalla()
+
+
+app()
