0a80c82d49510a12315d35817e97365a9bab501c
gusgluna <gusgluna@gmail.com>
2020-11-10 19:39:27 +0000

    gusgluna

--- Ejercicios_con_numeros/suma_de_cuadrados.py
+++ Ejercicios_con_numeros/suma_de_cuadrados.py
@@ -1,7 +1,14 @@
+'''
+Crear un programa que teng una funcion que reciba una lista de numeros, 
+y nos regrese la suma de sus cuadrados
+Por ejemplo: [0,1,2]= 0^2 + 1^2 + 2^2 = 5
+'''
 def square_sum(numbers):
     r=0
     for num in numbers:
         r= r + num**2
     print(r)
 
-square_sum([0,3,4,5])
+square_sum([0,1,2])
+
+square_sum([0,4,5,7])
